c7124b6fc370fafb19a2e160827b2a2c25452249
Evan Heidtmann <evan.heidtmann@gmail.com>
2016-06-21 12:39:03 -0700

    CSV Export

--- heidtco/rsvp/admin.py
+++ heidtco/rsvp/admin.py
@@ -1,9 +1,19 @@
 from django.contrib import admin
 
-from . import models
-# Register your models here.
+from import_export import resources
+import import_export.admin
 
-class RSVPAdmin(admin.ModelAdmin):
+from . import models
+
+class RSVPResource(resources.ModelResource):
+    class Meta:
+        model = models.RSVP
+        import_id_fields = ('uuid',)
+        fields = ('uuid', 'created', 'modified', 'email', 'rsvp', 'details')
+
+class RSVPAdmin(import_export.admin.ExportMixin, admin.ModelAdmin):
+    resource_class = RSVPResource
+
     list_display = ('modified', 'email', 'rsvp', 'details')
     readonly_fields = ('first_seen', 'created', 'uuid', 'client_id')
 
